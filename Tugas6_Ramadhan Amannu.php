c2539593a71eb968ee271b540c6989eefbe061e3
ramadhanamannu103@gmail.com <96917381+RamadhanA1@users.noreply.github.com>
2022-04-14 19:30:20 +0700

    tambahan baru

--- Tugas6_Ramadhan Amannu.php
+++ Tugas6_Ramadhan Amannu.php
@@ -1,34 +1,86 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstraps -->
+    <link rel="stylesheet" href="style/css/bootstrap.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <div class="container">
-        <div class="nav-bar">
-            
+    <!-- Title -->
+    <title>Kalkulator Sederhana</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="#">MyApps</a>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Calkulator</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Converter</a>
+            </li>
         </div>
-    <h2>Kalkulator Sederhana</h2>
-    <form action="" method="post">
-        <input type="text" name="bil1" id="">
-        <select name="operasi" id="">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-            <option value="%">%</option>
-        </select>
-        <input type="text" name="bil2" id="">
-        <input type="submit" value="=">
-    </form>
-    <?php
+      </div>
+    </nav>      
+
+    <!-- Jumbotron -->
+    <div class="mt-5 p-5 text-white text-center" style="background-color: #709CF3;">
+      <h3>SIMPLE CALCULATOR</h3>
+    </div>
+
+
+    <!-- Body -->
+    <h3 class="container p-3">INPUT</h3>
+    <div class="container p-4 d-flex justify-content-center">
+      <form class="row g-4" action="" method="post">
+        <div class="col-auto">
+          <input class="form-control" type="text" name="bil1" placeholder="Bilangan Pertama">
+        </div>
+        <div class="col-auto">
+          <select class="form-select" name="operasi" id="">
+              <option selected>Pilih Operasi</option>
+              <option value="+">+</option>
+              <option value="-">-</option>
+              <option value="*">*</option>
+              <option value="/">/</option>
+              <option value="%">%</option>
+          </select>
+        </div>
+        <div class="col-auto">
+          <input class="form-control" type="text" name="bil2" placeholder="Bilangan Kedua">
+        </div>
+        <div class="col-auto">
+          <input class="btn btn-primary" type="submit" value="HASIL">
+        </div>      
+      </form>
+    </div>
+
+    <hr>
+    
+    <!-- Bottom Credit -->
+    <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-primary">
+      <div class="container">
+          <span class="navbar-text">2022. Made By Ramadhan Amannu.</span>
+      </div>
+    </nav> 
+
+    <!-- Operasi Hitung -->
+    <div class="container p-3">
+      <h3>RESULT</h3>
+      <div class="p-5 d-flex justify-content-center">
+        <p><strong>
+      <?php
         function penjumlahan(float $a, float $b){
             $c=$a+$b;
             echo $c;
@@ -69,22 +121,19 @@
             }
             else {
                 modulus($a,$b);
-            }
-            
-            
+            }   
         }
-        
-    ?>
+      ?>
+      </strong></p>
+      </div>
+    
     </div>
-    <!-- Optional JavaScript; choose one of the two! -->
+    
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+      
+
+
+    <!-- Javascripst -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
+</body>
 </html>
---
+++ Tugas7_Ramadhan Amanni.md
@@ -0,0 +1,65 @@
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#run-apps">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+Ini adalah program hasil dari Praktek Tugas ke-6 dari Pelatihan BPPTIK VSGA G2 JDW 7 dengan Nama "MyApps" yang berisi dari Kalkulator sederhana dan Converter yang akan dikembangkan nantinya.
+
+### Built With
+
+Pembuatan program ini menggunakan Aplikasi dan Frameworks sebagai berikut:
+* [Bootstrap](https://getbootstrap.com)
+* [Visual Studio Code](https://code.visualstudio.com/)
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+Sebelum anda memulai program, unduh terlebih dahulu file "Tugas6_Ramadhan Amannu.zip".
+
+### Run Apps
+
+1. Extract file "Tugas6_Ramadhan Amannu.zip" difolder yang anda inginkan
+2. Buka Folder yang sudah di extract pada Visual Studio Code
+   ```sh
+   git clone https://github.com/your_username_/Project-Name.git
+   ```
+3. Buka aplikasi pada browser dengan menjalankan Termainal gitbash terlebih dahulu
+   ```sh
+   php -S localhost:8000
+   ```
+4. Kemudian buka browser dan ketikan pada kolom url
+   ```sh
+   localhost:8000/Tugas6_Ramadhan Amannu.php
+   ```
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Cara penggunaan aplikasinya:
+1. Masukan nilai pertama pada kolom "Bilangan Pertama"
+2. Pilih operasi hitung pada select box "Pilih Operasi"
+3. Masukan nilai pertama pada kolom "Bilangan Kedua"
+4. Klik button "HASIL" untuk menampilkan hasil perhitungan
